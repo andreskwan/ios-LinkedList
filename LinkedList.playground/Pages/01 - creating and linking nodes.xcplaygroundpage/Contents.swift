f7f3550307adec9b0db3b17348c5f946874a00b4
Andres Kwan <andreskwan@gmail.com>
2020-09-17 14:41:02 -0500

    [Nodes] - this is why we need a LinkedList type

--- LinkedList.playground/Pages/01 - creating and linking nodes.xcplaygroundpage/Contents.swift
+++ LinkedList.playground/Pages/01 - creating and linking nodes.xcplaygroundpage/Contents.swift
@@ -3,13 +3,15 @@
 import Foundation
 
 example(of: "creating and linking nodes") {
-  let node1 = Node(value: 1)
-  let node2 = Node(value: 2)
-  let node3 = Node(value: 3)
-  
-  node1.next = node2
-  node2.next = node3
-  
-  print(node1)
+    let node1 = Node(value: 1)
+    let node2 = Node(value: 2)
+    let node3 = Node(value: 3)
+        
+    //create a list in this way is impractical
+    //that is why we need a LinkedList that manages the nodes
+    node1.next = node2
+    node2.next = node3
+    
+    print(node1)
 }
 //: [Next](@next)
